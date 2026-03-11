0107e9b729d1f191e40077c14ce4a42ccd8ee3c6
James-the-coder <57504679+James-the-coder@users.noreply.github.com>
2026-03-11 07:59:06 +0000

    Add files via upload
    fixed network initialisations

--- SAC/RND/rnd.py
+++ SAC/RND/rnd.py
@@ -2,7 +2,13 @@
 # https://github.com/vwxyzjn/cleanrl/blob/master/cleanrl/ppo_rnd_envpool.py
 
 import torch.nn as nn
+import numpy as np
+import torch
 
+def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
+    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.constant_(layer.bias, bias_const)
+    return layer
 
 class RNDModel(nn.Module):
     def __init__(self, input_size):
@@ -12,24 +18,24 @@ class RNDModel(nn.Module):
 
         # predictor network
         self.predictor_net = nn.Sequential(
-            nn.Linear(self.input_size, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512)
+            layer_init(nn.Linear(self.input_size, 512)),
+            nn.LeakyReLU(),
+            layer_init(nn.Linear(512, 512)),
+            nn.LeakyReLU(),
+            layer_init(nn.Linear(512, 512)),
+            nn.LeakyReLU(),
+            layer_init(nn.Linear(512, 512))
         )
 
         # target network
         self.target_net = nn.Sequential(
-            nn.Linear(self.input_size, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512)
+            layer_init(nn.Linear(self.input_size, 512)),
+            nn.LeakyReLU(),
+            layer_init(nn.Linear(512, 512)),
+            nn.LeakyReLU(),
+            layer_init(nn.Linear(512, 512)),
+            nn.LeakyReLU(),
+            layer_init(nn.Linear(512, 512))
         )
 
         # freeze target network params
